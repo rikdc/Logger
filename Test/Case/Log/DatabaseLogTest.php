dc81742b6756a2b38680e66da6a0a0e2ae841f94
Richard Claydon <richard@icdw.co.uk>
2012-10-29 14:20:16 +0000

    Update Test/Case/Log/DatabaseLogTest.php

--- Test/Case/Log/DatabaseLogTest.php
+++ Test/Case/Log/DatabaseLogTest.php
@@ -1,18 +1,9 @@
 <?php
 /**
- * FileLogTest file
+ * DatabaseLogTest file
  *
  * PHP 5
  *
- * CakePHP(tm) Tests <http://book.cakephp.org/view/1196/Testing>
- * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice
- *
- * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://book.cakephp.org/view/1196/Testing CakePHP(tm) Tests
- * @package       Cake.Test.Case.Log.Engine
  * @since         CakePHP(tm) v 1.3
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
