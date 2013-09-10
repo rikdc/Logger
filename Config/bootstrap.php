ed7b8a915ab9e3b8e8100e21a42dff777508507c
Richard Claydon <richard@icdw.co.uk>
2013-09-10 09:38:44 +0100

    Update bootstrap.php
    Removed default configuration as this makes an unreasonable assumption for projects using this plugin.

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,10 +1,2 @@
 <?php
-$options = array('engine' => 'Logger.EmailLogger');
-
-try {
-    Configure::load('Logger.config');
-    $options = array_merge(Configure::read('EmailLogger'), $options);
-} catch (ConfigureException $e) {}
-
 App::uses('CakeLog', 'Log');
-CakeLog::config('email', $options);
